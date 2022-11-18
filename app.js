241678b0b8d3fd8aec9584e761c9cc42dfe358e8
Anna Efimova <annaryabko@gmail.com>
2022-11-18 19:42:20 +0100

    addc

--- app.js
+++ app.js
@@ -17,6 +17,34 @@ mongoose.connect(process.env.DB || 'mongodb://localhost:27017/bitfilmsdb', {
 
 const { PORT = 3000 } = process.env;
 
+const allowedCors = [
+  'https://anna.nomorepartiesxyz.ru',
+  'http://anna.nomorepartiesxyz.ru',
+  'http://localhost:3000',
+  'http://localhost:3001',
+];
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+app.use((req, res, next) => {
+  const { method } = req;
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  const requestHeaders = req.headers['access-control-request-headers'];
+  // проверяем, что источник запроса есть среди разрешённых
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.end();
+    return;
+  }
+
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
 app.use(cookieParser());
